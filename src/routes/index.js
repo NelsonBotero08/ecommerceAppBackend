da9522a73d115606fdd4673a9b9ea6604baeb6a1
Cristian Bastidas <cbastidas929@gmail.com>
2024-01-21 20:36:47 -0500

    create product, category, talla

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const userRouter = require("./user.router");
+const productRouter = require("./product.router");
+const categoryRouter = require("./category.router");
+const tallaRouter = require("./talla.router");
 const router = express.Router();
 
 // colocar las rutas aquí
 router.use(userRouter);
+router.use('/products',productRouter)
+router.use('/categories',categoryRouter)
+router.use('/tallas',tallaRouter)
 
 module.exports = router;
